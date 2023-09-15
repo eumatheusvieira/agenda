f8b1c4aa0aaf3cd1d99aa28f638990a3f9a91cf2
Matheus Vieira <matheusvrgo2@gmail.com>
2023-09-15 10:01:09 -0300

    Incluido sistema de alteração e exclusão de dados

--- core/models.py
+++ core/models.py
@@ -17,3 +17,6 @@ class Evento(models.Model):
     
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y - %H:%M')
+    
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
